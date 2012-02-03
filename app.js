30390febe52ee35be16f74412ba28f372a8c3e76
Łukasz Twarogowski <lukasz@mon1.(none)>
2012-02-03 19:01:57 +0100

    modules added

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ var fs = require('fs'),
 	_ = require('underscore'),
 	util = require('util'),
 	cfg = require('./config');
+	zipdb = require('./lib/zip-db');
+
+zipdb.importFromFile(cfg.importFilePath);
+
 
